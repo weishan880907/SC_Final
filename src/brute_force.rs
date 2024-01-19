b5d63b55eda5083fe09e81028d385d1a0de5a25b
weishan <weishan880907@gmail.com>
2024-01-19 14:19:11 +0100

    Update brute_force without num-bigint

--- src/brute_force.rs
+++ src/brute_force.rs
@@ -6,9 +6,7 @@ fn is_prime(n: u64) -> bool {
         return false;
     }
 
-    let sqrt_n = (n as f64).sqrt() as u64;
-
-    for i in 2..=sqrt_n {
+    for i in 2..= n-1 {
         if n % i == 0 {
             return false;
         }
@@ -18,21 +16,21 @@ fn is_prime(n: u64) -> bool {
 }
 
 fn main() {
-    // Get the input from the environment variable
-    let input_str = env::var("NUM_TO_TEST").unwrap_or_else(|_| String::from("23"));
+    // Get the input from the command line.
+    let args: Vec<String> = env::args().collect();
 
-    // Parse the input as u64
-    let num_to_test: u64 = input_str.parse().unwrap_or_else(|_| {
-        eprintln!("Invalid input. Using default value: 23");
-        23
-    });
+    // Parse the input as u64 and set default value as 23.
+    let num_to_test: u64 = args.get(1)
+        .and_then(|arg| arg.parse().ok())
+        .unwrap_or_else(|| {
+            eprintln!("Invalid or missing input. Using default value: 23");
+            23
+        });
 
-    // Check if the number is prime
+    // Check if the number is prime. There will not be probable prime here.
     if is_prime(num_to_test) {
         println!("{} is prime.", num_to_test);
     } else {
-        // In this simple example, we're considering any non-prime as composite.
-        // You might want to use a more sophisticated algorithm for larger inputs.
         println!("{} is composite.", num_to_test);
     }
 }
