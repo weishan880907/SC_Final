204e27f3cbe5f35d8aa00478e488a5cf2cf84e9b
weishan <weishan880907@gmail.com>
2024-01-29 16:47:16 +0100

    revised fermat_little_theorem

--- src/fermat_little_theorem.rs
+++ src/fermat_little_theorem.rs
@@ -1,12 +1,11 @@
-// 499263 Wei-Shan Chang
-
 use std::{env, time};
 use num_bigint::BigUint;
 use num_traits::{One, Zero, ToPrimitive};
 use rand::Rng;
 
 fn mod_exp(base: &BigUint, exponent: &BigUint, modulus: &BigUint) -> BigUint {
-    if modulus == &BigUint::one() {
+    let mod_exp_time = time::Instant::now();
+    if modulus.is_one() {
         return BigUint::zero();
     }
 
@@ -18,25 +17,44 @@ fn mod_exp(base: &BigUint, exponent: &BigUint, modulus: &BigUint) -> BigUint {
         result = (result * &base) % modulus;
         current_exp += BigUint::one();
     }
+    println!("Mod Exp Time: {:?}", mod_exp_time.elapsed());
 
     result
 }
 
+fn gcd(a: &BigUint, b: &BigUint) -> BigUint {
+    if a.is_zero() {
+        b.clone()
+    } else {
+        gcd(&(b % a), a)
+    }
+}
+
 fn is_prime_fermat(n: &BigUint, k: usize) -> bool {
-    if *n <= BigUint::one() {
+    let is = time::Instant::now();
+    if n.is_one() {
         return false;
     }
 
     let mut rng = rand::thread_rng();
 
     for _ in 0..k {
-        let a_u64: u64 = rng.gen_range(1..n.to_u64().unwrap_or(u64::MAX));
-        if mod_exp(&BigUint::from(a_u64), &(n - BigUint::one()), n) != BigUint::one() {
+        let a_u64: u64 = rng.gen_range(2..n.to_u64().unwrap_or(u64::MAX));
+        let a = BigUint::from(a_u64);
+        println!("{}, {}", a_u64,a );
+        // Check gcd(a, n) != 1
+        if gcd(&a, n) != BigUint::one() {
+            return false;
+        }
+
+        // Check a^(n-1) ≡ 1 (mod n)
+        if mod_exp(&a, &(n - BigUint::one()), n) != BigUint::one() {
             return false;
         }
     }
-
+    println!("is fermat Time: {:?}", is.elapsed());
     true
+    
 }
 
 fn main() {
