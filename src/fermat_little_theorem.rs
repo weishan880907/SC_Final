3dfdd2468258e0bb5fb49351b6827131d788a21e
weishan <weishan880907@gmail.com>
2024-01-29 15:57:18 +0100

    Revised fermat_little_theorem

--- src/fermat_little_theorem.rs
+++ src/fermat_little_theorem.rs
@@ -1,33 +1,37 @@
 // 499263 Wei-Shan Chang
-// TODO: add the restriction of input with Big Int.
 
-use std::env;
+use std::{env, time};
+use num_bigint::BigUint;
+use num_traits::{One, Zero, ToPrimitive};
+use rand::Rng;
 
-fn mod_exp(base: u64, exponent: u64, modulus: u64) -> u64 {
-    if modulus == 1 {
-        return 0;
+fn mod_exp(base: &BigUint, exponent: &BigUint, modulus: &BigUint) -> BigUint {
+    if modulus == &BigUint::one() {
+        return BigUint::zero();
     }
 
-    let mut result = 1;
-
-    // This operation ensures that base remains within the range of 0 to modulus - 1.
+    let mut result = BigUint::one();
     let base = base % modulus;
 
-    for _ in 0..exponent {
-        result = (result * base) % modulus;
+    let mut current_exp = BigUint::zero();
+    while current_exp < *exponent {
+        result = (result * &base) % modulus;
+        current_exp += BigUint::one();
     }
 
     result
 }
 
-fn is_prime_fermat(n: u64, k: u64) -> bool {
-    if n <= 1 {
+fn is_prime_fermat(n: &BigUint, k: usize) -> bool {
+    if *n <= BigUint::one() {
         return false;
     }
 
+    let mut rng = rand::thread_rng();
+
     for _ in 0..k {
-        let a = rand::random::<u64>() % (n - 1) + 1; // Randomly choose 'a' between 1 and n-1
-        if mod_exp(a, n - 1, n) != 1 {
+        let a_u64: u64 = rng.gen_range(1..n.to_u64().unwrap_or(u64::MAX));
+        if mod_exp(&BigUint::from(a_u64), &(n - BigUint::one()), n) != BigUint::one() {
             return false;
         }
     }
@@ -39,21 +43,25 @@ fn main() {
     // Get the input from the command line.
     let args: Vec<String> = env::args().collect();
 
-    // Parse the input as u64 and set default value as 23.
-    let num_to_test: u64 = args.get(1)
+    // Parse the input as BigUint and set the default value as 23.
+    let num_to_test: BigUint = args.get(1)
         .and_then(|arg| arg.parse().ok())
         .unwrap_or_else(|| {
             eprintln!("Invalid or missing input. Using default value: 23");
-            23
+            BigUint::from(23u32)
         });
 
     // Choose a value of k, which determines the number of tests.
     let k = 5;
 
-    // Check if the number is prime by using Fermat's Little Theorem.
-    if is_prime_fermat(num_to_test, k) {
+    // Measure the time taken to check if the number is probably prime.
+    let start_time = time::Instant::now();
+    if is_prime_fermat(&num_to_test, k) {
         println!("{} is probably prime.", num_to_test);
     } else {
         println!("{} is composite.", num_to_test);
     }
+    let elapsed_time = start_time.elapsed();
+
+    println!("Time taken: {:?}", elapsed_time);
 }
