7586c8cd60044f79a817670025455a95fd32004f
weishan <weishan880907@gmail.com>
2024-01-29 15:09:32 +0100

    brute_force revised

--- src/brute_force.rs
+++ src/brute_force.rs
@@ -1,17 +1,20 @@
-// 499263 Wei-Shan Chang
-// TODO: add the restriction of input with Big Int.
-
 use std::env;
+use num_bigint::BigUint;
+use num_traits::{One, Zero};
 
-fn is_prime(n: u64) -> bool {
-    if n <= 1 {
+fn is_prime(n: &BigUint) -> bool {
+    if n <= &BigUint::one() {
         return false;
     }
 
-    for i in 2..= n-1 {
-        if n % i == 0 {
+    let mut i = BigUint::from(2u32);
+    let sqrt_n = n.sqrt();
+
+    while &i <= &sqrt_n {
+        if n % &i == BigUint::zero() {
             return false;
         }
+        i += BigUint::one();
     }
 
     true
@@ -21,18 +24,22 @@ fn main() {
     // Get the input from the command line.
     let args: Vec<String> = env::args().collect();
 
-    // Parse the input as u64 and set default value as 23.
-    let num_to_test: u64 = args.get(1)
+    // Parse the input as BigUint and set default value as 23.
+    let num_to_test: BigUint = args.get(1)
         .and_then(|arg| arg.parse().ok())
         .unwrap_or_else(|| {
             eprintln!("Invalid or missing input. Using default value: 23");
-            23
+            BigUint::from(23u32)
         });
 
-    // Check if the number is prime. There will not be probably prime here.
-    if is_prime(num_to_test) {
+    // Measure the time taken to check if the number is prime.
+    let start_time = std::time::Instant::now();
+    if is_prime(&num_to_test) {
         println!("{} is prime.", num_to_test);
     } else {
         println!("{} is composite.", num_to_test);
     }
+    let elapsed_time = start_time.elapsed();
+
+    println!("Time taken: {:?}", elapsed_time);
 }
